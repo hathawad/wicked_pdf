70cf3f16b2434f837550944811d1f2b1eb34a28e
Doug Hathaway <doughathaway@gmail.com>
2012-07-12 13:44:42 -0400

    update for assets path

--- lib/wicked_pdf_helper.rb
+++ lib/wicked_pdf_helper.rb
@@ -1,17 +1,17 @@
 module WickedPdfHelper
   def wicked_pdf_stylesheet_link_tag(*sources)
-    css_dir = Rails.root.join('public','stylesheets')
+    css_dir = Rails.root.join('assets')
     sources.collect { |source|
       "<style type='text/css'>#{File.read( tomcat_path(css_dir.join(source+'.css').to_s) )}</style>"
     }.join("\n").html_safe
   end
 
   def wicked_pdf_image_tag(img, options={})
-    image_tag "file://#{ tomcat_path(Rails.root.join('public', 'images', img).to_s)}", options
+    image_tag "file://#{ tomcat_path(Rails.root.join('assets', img).to_s)}", options
   end
 
   def wicked_pdf_javascript_src_tag(jsfile, options={})
-    javascript_src_tag "file://#{ tomcat_path(Rails.root.join('public','javascripts',jsfile).to_s) }", options
+    javascript_src_tag "file://#{ tomcat_path(Rails.root.join('assets',jsfile).to_s) }", options
   end
 
   def wicked_pdf_javascript_include_tag(*sources)
@@ -20,7 +20,7 @@ module WickedPdfHelper
 
 
   def tomcat_path(path_string)
-    path_string.gsub!("WEB-INF/public","")
+    path_string.gsub!("WEB-INF/","")
   end
   private :tomcat_path
 
